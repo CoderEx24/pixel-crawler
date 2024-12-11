ae44ef52be83c0cd3c8fa54d870f13c7f3144371
CoderEx24 <coderex24@protonmail.com>
2024-12-11 14:06:04 +0200

    added more files

--- hero.gd
+++ hero.gd
@@ -3,23 +3,38 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var flip = false
 
+func _entered_exit(body: Node2D):
+	if body.name != 'Hero':
+		return
+	get_tree().quit()
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	velocity = Vector2(0, 0)
+	
+	if Input.is_action_pressed('ui_left'):
+		velocity.x = -SPEED
+	if Input.is_action_pressed('ui_right'):
+		velocity.x = SPEED
+	if Input.is_action_pressed('ui_up'):
+		velocity.y = -SPEED
+	if Input.is_action_pressed('ui_down'):
+		velocity.y = SPEED
+		
 	move_and_slide()
+
+func _process(delta):
+	var animation = 'idle'
+	if velocity.length() > 0:
+		animation = 'run'
+	
+	if Input.is_action_pressed('ui_left'):
+		flip = true
+	if Input.is_action_pressed('ui_right'):
+		flip = false
+	
+	$AnimatedSprite2D.flip_h = flip
+	
+	$AnimatedSprite2D.play(animation)
