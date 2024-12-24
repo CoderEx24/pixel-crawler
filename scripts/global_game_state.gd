b763f684e02b9fb41c264ed2b908c09de5230088
CoderEx24 <coderex24@protonmail.com>
2024-12-24 10:23:50 +0200

    connect all scenes together

--- scripts/global_game_state.gd
+++ scripts/global_game_state.gd
@@ -1,18 +1,18 @@
 extends Node
 
-var _selected_hero: String
+var _selected_hero: String = ''
 
 func _ready():
 	pass
 
 func select_hero(h: String):
 	var hero = h.to_lower()
-	if hero not in ['knight', 'wizard', 'rogue']:
-		assert('Unknown hero type %s' % hero)
+	assert(hero in ['knight', 'wizard', 'rogue'], 'Unknown hero type %s' % hero)
 	
 	_selected_hero = hero
 
 func instantiate_hero():
+	assert(_selected_hero in ['knight', 'wizard', 'rogue'], 'Unknown hero type %s' % _selected_hero)
 	return load('res://scenes/core/%s.tscn' % _selected_hero).instantiate()
 
 func selected_hero():
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,7 +5,7 @@ func _ready():
 	pass
 	
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file('res://levels_selection.tscn')
+	get_tree().change_scene_to_file('res://scenes/hero_selection_menu.tscn')
 	
 func _on_exit_pressed()  -> void:
 	get_tree().quit()
