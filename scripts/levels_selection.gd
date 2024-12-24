40aea9f20fbca67a40d5fbb3d8e3737cc2ff21bb
CoderEx24 <coderex24@protonmail.com>
2024-12-24 09:42:56 +0200

    refactor level selection scene

--- scripts/levels_selection.gd
+++ scripts/levels_selection.gd
@@ -1,24 +1,5 @@
-class_name LevelSelection
 extends Control
 
-@onready var level1_button = $MarginContainer/HBoxContainer/VBoxContainer/Level1_button as Button
-@onready var level2_button = $MarginContainer/HBoxContainer/VBoxContainer/Level2_button as Button
-@onready var level3_button = $MarginContainer/HBoxContainer/VBoxContainer/Level3_button as Button
-
-@onready var start_level1 = preload("res://scenes/Level2.tscn") as PackedScene
-@onready var start_level2 = preload("res://scenes/Level2.tscn") as PackedScene
-@onready var start_level3 = preload("res://scenes/Level2.tscn") as PackedScene
-func _ready():
-	level1_button.button_down.connect(on_level1_pressed)
-	level2_button.button_down.connect(on_level2_pressed)
-	level3_button.button_down.connect(on_level3_pressed)
-
-
-func on_level1_pressed()  -> void:
-	get_tree().change_scene_to_packed(start_level1)
-	
-func on_level2_pressed() -> void:
-	get_tree().change_scene_to_packed(start_level2)
-	
-func on_level3_pressed()  -> void:
-	get_tree().change_scene_to_packed(start_level3)
+func _on_level_selection(level: StringName) -> void:
+	var scene_name = 'res://scenes/levels/%s.tscn' % level
+	get_tree().change_scene_to_file(scene_name)
