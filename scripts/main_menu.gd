ddec97d221406e85ae8a10681ba4b38215e6c757
CoderEx24 <coderex24@protonmail.com>
2024-12-24 09:29:00 +0200

    refector main menu scene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,16 +1,11 @@
 class_name MainMenu
 extends Control
 
-@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_button as Button
-@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_button as Button
-@onready var start_level = preload("res://scenes/menu.tscn") as PackedScene
-
 func _ready():
-	start_button.button_down.connect(on_start_pressed)
-	exit_button.button_down.connect(on_exit_pressed)
+	pass
 	
-func on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(start_level)
-
-func on_exit_pressed()  -> void:
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file('res://levels_selection.tscn')
+	
+func _on_exit_pressed()  -> void:
 	get_tree().quit()
