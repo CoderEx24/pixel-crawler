157b2daf2ad365fead16807dd77bb0ca99172ecd
CoderEx24 <coderex24@protonmail.com>
2024-12-30 04:09:29 +0200

    spawn a collectable near an opened chest

--- scripts/base_level.gd
+++ scripts/base_level.gd
@@ -20,10 +20,23 @@ func _process(delta: float) -> void:
 	
 func _on_tile_hit(kind: String, coords: Vector2):
 	print('hit ', kind, ' at ', coords)
-	$Props.set_cell(coords, 0, Vector2(3, 13))
-	$Props.set_cell(coords + Vector2.UP, 0, Vector2(3, 12))
+	match kind:
+		'crate':
+			$Props.set_cell(coords, 0, Vector2(3, 13))
+			$Props.set_cell(coords + Vector2.UP, 0, Vector2(3, 12))
+			
+			var collectable_position = $Props.to_global(
+				$Props.map_to_local(coords + 2 * Vector2.UP + 2 * Vector2.RIGHT)
+			)
+			var collectable = preload('res://scenes/core/collectable.tscn').instantiate()
+			collectable.global_position = collectable_position
+			collectable.connect('collected', _on_collectable_collected)
+			
+			add_child(collectable)
 	
-	
+
+func _on_collectable_collected(collectable: String):
+	print('collected ', collectable)
 
 func _on_enemy_died():
 	hero.score += 100
--- scripts/hero_melee_weapon.gd
+++ scripts/hero_melee_weapon.gd
@@ -42,6 +42,7 @@ func _physics_process(_delta: float) -> void:
 
 			var kind = tile.get_custom_data_by_layer_id(0)
 			emit_signal('tile_hit', kind, tilecoords)
+			$Timeout.start()
 		elif collider is CharacterBody2D:
 			if collider is Enemy and _state == MeleeWeaponState.ATTACKING:
 				var damage_multiplier = 1.2 if _kind == MeleeAttackType.STRONG else 1.0
